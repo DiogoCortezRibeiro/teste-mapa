1523429bdaacedb28831bf5de345bf39665c2f2e
diogo.ribeiro <diogoparceiros@brkambiental.com.br>
2022-09-22 16:37:34 -0300

    Adicionando configurações

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapboxGL from '@rnmapbox/maps';
 
-MapboxGL.setAccessToken('pk.eyJ1IjoiZGlvbmVzYW50b3MiLCJhIjoiY2w4OWUwaWE2MDY2dDNycGRlMnNsbDNsZyJ9.FnBNs4QpS3HigVQ3Cmk3aw');
 
 const App = () => {
   return (
     <View style={styles.page}>
       <View style={styles.container}>
-        <MapboxGL.MapView style={styles.map} />
       </View>
     </View>
   );
